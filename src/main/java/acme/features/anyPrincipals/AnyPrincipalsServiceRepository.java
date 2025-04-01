d40c293dbef6b9c5bc8b9b67d49a32297d45ebe9
aloporsan <alonsoportillo.sanchez@gmail.com>
2025-04-01 09:37:43 +0200

    D03-G-14: Fix anyprincipals services random image

--- src/main/java/acme/features/anyPrincipals/AnyPrincipalsServiceRepository.java
+++ src/main/java/acme/features/anyPrincipals/AnyPrincipalsServiceRepository.java
@@ -1,24 +1,48 @@
 
 package acme.features.anyPrincipals;
 
-import java.util.Collection;
+import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import acme.client.helpers.RandomHelper;
 import acme.client.repositories.AbstractRepository;
 import acme.entities.Services.Service;
 
 @Repository
 public interface AnyPrincipalsServiceRepository extends AbstractRepository {
 
+	@Query("select count(s) from Service s")
+	int countServices();
+
 	@Query("select s from Service s")
-	Collection<Service> findAllServices();
+	List<Service> findAllServices(PageRequest pageRequest);
 
 	@Query("select s from Service s where s.id = :id")
 	Service findServiceById(int id);
 
-	@Query(value = "SELECT * FROM service ORDER BY RAND() LIMIT 1", nativeQuery = true)
-	Service findRandomService();
+	default Service findRandomService() {
+		Service result;
+		int count, index;
+		PageRequest page;
+		List<Service> list;
+
+		count = this.countServices();
+		if (count == 0)
+			result = null;
+		else {
+			index = RandomHelper.nextInt(0, count);
+			page = PageRequest.of(index, 1, Sort.by(Direction.ASC, "id"));
+			list = this.findAllServices(page);
+			result = list.isEmpty() ? null : list.get(0);
+		}
+
+		return result;
+
+	}
 
 }
